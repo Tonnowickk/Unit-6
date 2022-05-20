9802e49bdea15740af3e49ae1cb8315d27c1aa4c
Tonnowickk <105931129+Tonnowickk@users.noreply.github.com>
2022-05-20 09:21:49 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+$(".hidden").hide();
+
+$(".yes-button").click(function() {
+$(".Part-Two-Yes").show();
+$(".home").hide();
+    
+});
+
+$(".no-button").dblclick(function() {
+$(".Part-Two-No").show();
+$(".home").hide();
+    
+});
+
+$(".title").click(function() {
+$("body").css("background-color", "black");
+
+    
+});
+
